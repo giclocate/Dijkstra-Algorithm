5336ebed53fab3cd1f13bcc3a695eeec67dd66dd
Giovanna Clócate <giovannacca@hotmail.com>
2024-07-13 13:44:49 -0300

    Ajustes do banco

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import networkx as nx
 from grafo import load_graph_from_file, dijkstra
 
 def main():
-    caminho_arquivo = 'ca-sandi_auths.mtx'
+    caminho_arquivo = 'Dijkstra-Algorithm/aves-geese-female-foraging.edges'
    
     if not os.path.exists(caminho_arquivo):
         print(f"Arquivo {caminho_arquivo} não existe.")
@@ -20,13 +20,15 @@ def main():
     if no_destino not in distancias:
         print(f"Nenhum caminho encontrado do nó {no_origem} para o nó {no_destino}.")
     else:
-        print(f"Menor distância do nó {no_origem} para o nó {no_destino}: {distancias[no_destino]}")
-        print(f"Caminho: {' -> '.join(map(str, caminhos[no_destino]))}")
+        distancia = distancias[no_destino]
+        caminho = caminhos[no_destino]
+        print(f"Menor distância do nó {no_origem} para o nó {no_destino}: {distancia}")
+        print(f"Caminho: {' -> '.join(map(str, caminho))}")
        
-        visualizar_grafo(grafo, caminhos[no_destino], no_origem, no_destino)
+        visualizar_grafo(grafo, caminho, no_origem, no_destino, distancia)
 
-def visualizar_grafo(grafo, caminho, no_origem, no_destino):
-    pos = nx.spring_layout(grafo, seed=32, k=7.5)  # Usando o layout spring_layout
+def visualizar_grafo(grafo, caminho, no_origem, no_destino, distancia):
+    pos = nx.spring_layout(grafo, seed=32, k=7.5, iterations=100)  # Usando o layout spring_layout
     plt.figure(figsize=(14, 14))
    
     nx.draw_networkx_edges(grafo, pos, edge_color='lightgray', width=0.5)
@@ -37,7 +39,11 @@ def visualizar_grafo(grafo, caminho, no_origem, no_destino):
    
     nx.draw_networkx_labels(grafo, pos, font_size=10, font_color='black')
    
-    plt.title(f'Visualização do Grafo\nMenor caminho do nó {no_origem} ao nó {no_destino}', fontsize=15)
+    # Draw edge labels for weights on the path only
+    edge_labels = {(caminho[i], caminho[i+1]): grafo[caminho[i]][caminho[i+1]]['weight'] for i in range(len(caminho) - 1)}
+    nx.draw_networkx_edge_labels(grafo, pos, edge_labels=edge_labels, font_color='black')
+   
+    plt.title(f'Visualização do Grafo\nMenor caminho do nó {no_origem} ao nó {no_destino}\nA menor distância: {distancia}', fontsize=15)
     plt.axis('off')
     plt.show()
 
